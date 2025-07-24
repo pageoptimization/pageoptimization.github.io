b018f818149356a7f3ba621a3eac0a41225405d0
pageoptimization <daiki322.0101@gmail.com>
2025-07-24 12:54:12 +0900

    Delete ninsikigaibu.js

--- ninsikigaibu.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var script = document.createElement('script');
-  script.setAttribute('data-ad-client', 'ca-pub-5005673256143644');
-  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-  script.async = false;
-  script.crossOrigin = 'anonymous'; // ← ここで crossOrigin 属性を追加
-  document.head.appendChild(script);
-})();
