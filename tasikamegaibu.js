fa3dc26233cc955534d6189e988c66eaa3fddfc1
pageoptimization <daiki322.0101@gmail.com>
2025-07-29 15:17:03 +0900

    Add files via upload

---
+++ tasikamegaibu.js
@@ -0,0 +1,9 @@
+const adClient2 = 'ca-pub-8005772259143249';
+
+(function() {
+  const script = document.createElement('script');
+  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + encodeURIComponent(adClient2);
+  script.async = false;
+  script.crossOrigin = 'anonymous';
+  document.head.appendChild(script);
+})();
