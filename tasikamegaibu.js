74260f269d220756ace0cdbdd103a5a866d751cb
pageoptimization <daiki322.0101@gmail.com>
2025-08-23 03:30:08 +0900

    Delete tasikamegaibu.js

--- tasikamegaibu.js
+++
@@ -1,9 +0,0 @@
-const adClient2 = 'ca-pub-8005772259143249';
-
-(function() {
-  const script = document.createElement('script');
-  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + encodeURIComponent(adClient2);
-  script.async = false;
-  script.crossOrigin = 'anonymous';
-  document.head.appendChild(script);
-})();
